a9cba0961ab9c82fa8fa9ee4362836d42941b140
Milad <miladev95@gmail.com>
2025-12-22 12:30:00 +0330

    Add version subcommand to report gorm-cli build version (#33)
    * feat: add version command to CLI
    
    * use runtime/debug build info for version
    
    * level added
    
    * Inline version resolution into `versionCmd`; remove top-level `getVersion`

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 	"gorm.io/cli/gorm/internal/gen"
@@ -15,9 +16,27 @@ func main() {
 	}
 
 	rootCmd.AddCommand(gen.New())
+	rootCmd.AddCommand(versionCmd())
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func versionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print the version of gorm-cli",
+		Run: func(cmd *cobra.Command, args []string) {
+			if info, ok := debug.ReadBuildInfo(); ok {
+				if info.Main.Version != "" && info.Main.Version != "(devel)" {
+					fmt.Printf("gorm-cli version %s\n", info.Main.Version)
+					return
+				}
+			}
+
+			fmt.Printf("gorm-cli version %s\n", "dev")
+		},
 	}
 }
